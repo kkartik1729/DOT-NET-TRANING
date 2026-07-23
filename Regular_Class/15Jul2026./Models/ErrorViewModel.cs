b5d4523f9f0e21c655dfa9e0ef5219db6ef8bc21
Kartik Hande <handekartik181@gmail.com>
2026-07-23 16:55:14 +0530

    Add files via upload

---
+++ Regular_Class/15Jul2026./Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _15Jul2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
