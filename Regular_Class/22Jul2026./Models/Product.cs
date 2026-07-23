4b8312eb106598217d6b3b2d72797bdcf41f058f
Kartik Hande <handekartik181@gmail.com>
2026-07-23 17:14:41 +0530

    Add files via upload

---
+++ Regular_Class/22Jul2026./Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _22Jul2026.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is Mandatory")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is Mandatory")]
+        [Range(10, 100000)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Category is Mandatory")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Stock is Mandatory")]
+        public int Stock { get; set; }
+    }
+}
---
+++ Regular_Class/22Jul2026./Models/Stationary.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _22Jul2026.Models
+{
+    public class Stationary
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Item Name is Mandatory")]
+        public string ItemName { get; set; }
+
+        [Required(ErrorMessage = "Price is Mandatory")]
+        [Range(1, 10000, ErrorMessage = "Price must be between 1 and 10000")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Brand is Mandatory")]
+        public string Brand { get; set; }
+
+        [Required(ErrorMessage = "Quantity is Mandatory")]
+        public int Quantity { get; set; }
+    }
+}
