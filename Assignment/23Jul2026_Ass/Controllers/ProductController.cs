85a17c80d0ec7ad052f3f194403494e01faebc51
Kartik Hande <handekartik181@gmail.com>
2026-07-24 09:30:16 +0530

    Add files via upload

---
+++ Assignment/23Jul2026_Ass/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using _23Jul2026.Models;
+using _23Jul2026.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _23Jul2026.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("User")))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            List<Product> products = new List<Product>()
+            {
+                new Product { id = 1, Name = "Pen", Price = 10 },
+                new Product { id = 2, Name = "Pencil", Price = 5 },
+                new Product { id = 3, Name = "Notebook", Price = 40 },
+                new Product { id = 4, Name = "Eraser", Price = 3 },
+                new Product { id = 5, Name = "Ruler", Price = 8 },
+                new Product { id = 6, Name = "Stapler", Price = 60 }
+            };
+
+            return View(products);
+        }
+    }
+}
---
+++ Assignment/23Jul2026_Ass/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _23Jul2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
