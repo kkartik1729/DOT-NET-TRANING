03d17da23bf572f261d6c1f88008e55e9b006fcc
Kartik Hande <handekartik181@gmail.com>
2026-08-13 20:11:04 +0530

    Refactor Program.cs for cleaner formatting

--- Assignment/08Aug2026_Ass./Program.cs
+++ Assignment/08Aug2026_Ass./Program.cs
@@ -2,7 +2,6 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
@@ -14,7 +13,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
