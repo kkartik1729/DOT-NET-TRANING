d40888a7043e52f00a11178bd197341b596695a4
Kartik Hande <handekartik181@gmail.com>
2026-08-13 20:08:04 +0530

    Format Course.cs and remove unnecessary comments

--- Assignment/08Aug2026_Ass./Models/Course.cs
+++ Assignment/08Aug2026_Ass./Models/Course.cs
@@ -17,14 +17,12 @@ namespace _08Aug2026_Ass.Models
         [Range(1, 24, ErrorMessage = "Duration must be between 1 and 24 months")]
         public int Duration { get; set; }
 
-        // One Teacher -> Many Courses (FK)
         [Required(ErrorMessage = "TeacherId is required")]
         public int TeacherId { get; set; }
 
         [ForeignKey(nameof(TeacherId))]
         public Teacher? Teacher { get; set; }
 
-        // Many Students <-> Many Courses
         [JsonIgnore]
         public ICollection<Student>? Students { get; set; } = new List<Student>();
     }
