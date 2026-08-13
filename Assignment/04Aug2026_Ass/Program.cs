f9717e0387c2c08e4934135f94c61cea54c84502
Kartik Hande <handekartik181@gmail.com>
2026-08-13 19:52:08 +0530

    Update Program.cs

--- Assignment/04Aug2026_Ass/Program.cs
+++ Assignment/04Aug2026_Ass/Program.cs
@@ -5,19 +5,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 
-//DI
+
 builder.Services.AddScoped<IStudentService, StudentService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
 
-//Swagger
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Database
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
