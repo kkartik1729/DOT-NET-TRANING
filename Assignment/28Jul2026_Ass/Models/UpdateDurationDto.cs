b3834f75cd51bb8c7d728dfcd90637a5e7522a6e
Kartik Hande <handekartik181@gmail.com>
2026-08-13 23:11:04 +0530

    Remove comments from UpdateDurationDto class
    Removed comments explaining the purpose of the UpdateDurationDto class.

--- Assignment/28Jul2026_Ass/Models/UpdateDurationDto.cs
+++ Assignment/28Jul2026_Ass/Models/UpdateDurationDto.cs
@@ -1,7 +1,5 @@
 namespace CourseRegistrationSystem.Models
 {
-    // Used by the "update course duration" endpoint, which only
-    // needs to change the duration field of an existing course.
     public class UpdateDurationDto
     {
         public int duration { get; set; }
