96d243a093b14a3751261ea349ec4347e2d1921b
Kartik Hande <handekartik181@gmail.com>
2026-08-13 21:28:15 +0530

    Update HomeController.cs

--- Assignment/23Jul2026_Ass/Controllers/HomeController.cs
+++ Assignment/23Jul2026_Ass/Controllers/HomeController.cs
@@ -6,16 +6,15 @@ namespace _23Jul2026.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: login 
+  
         public IActionResult Index()
         {
             return View();
         }
-        //POST: login 
+
         [HttpPost]
         public IActionResult Index(string username, string password)
         {
-            // simple hardcore login 
             if (username == "admin" && password == "12345")
             {
                 HttpContext.Session.SetString("User", username);
